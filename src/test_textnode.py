200b77bc07c6db8845919e1b2e69c88f11e77d53
charlesaraya <charlesaraya@gmail.com>
2025-04-01 16:46:20 +0200

    feat+test: split nodes with delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,20 +1,59 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        bold_node = TextNode("This is a test node", TextType.BOLD)
-        same_bold_node = TextNode("This is a test node", TextType.BOLD)
-        same_bold_node_and_link = TextNode("This is a test node", TextType.BOLD, "https://charlot.dev")
-        diff_bold_node = TextNode("This is a different test node", TextType.BOLD)
-        link_node = TextNode("This is a test node", TextType.LINK)
+    normal_node = TextNode("This is a text node", TextType.NORMAL)
+    bold_node = TextNode("This is a test node", TextType.BOLD)
+    same_bold_node = TextNode("This is a test node", TextType.BOLD)
+    same_bold_node_and_link = TextNode("This is a test node", TextType.BOLD, "https://charlot.dev")
+    diff_bold_node = TextNode("This is a different test node", TextType.BOLD)
+    link_node = TextNode("Click here!", TextType.LINK, "https://example.com")
 
-        self.assertEqual(bold_node, same_bold_node)
-        self.assertNotEqual(bold_node, diff_bold_node)
-        self.assertNotEqual(bold_node, same_bold_node_and_link)
-        self.assertNotEqual(bold_node, link_node)
-        self.assertEqual(bold_node.url, None)
+    def test_eq(self):
+        self.assertEqual(TestTextNode.bold_node, TestTextNode.same_bold_node)
+        self.assertNotEqual(TestTextNode.bold_node, TestTextNode.diff_bold_node)
+        self.assertNotEqual(TestTextNode.bold_node, TestTextNode.same_bold_node_and_link)
+        self.assertNotEqual(TestTextNode.bold_node, TestTextNode.link_node)
+        self.assertEqual(TestTextNode.bold_node.url, None)
+    
+    def test_node_to_html(self):
+        normal_html_node = text_node_to_html_node(TestTextNode.normal_node)
+        self.assertEqual(normal_html_node.tag, None)
+        self.assertEqual(normal_html_node.value, "This is a text node")
+
+        link_html_node = text_node_to_html_node(TestTextNode.link_node)
+        self.assertEqual(link_html_node.tag, "a")
+        self.assertEqual(link_html_node.value, 'Click here!')
+        self.assertEqual(link_html_node.props, {"href": "https://example.com"})
+
+        # A TextNode of type TextType.BOLD and url is converted to a LeafNode w/o props
+        not_link_html_node = text_node_to_html_node(TestTextNode.same_bold_node_and_link)
+        self.assertEqual(not_link_html_node.tag, "b")
+        self.assertEqual(not_link_html_node.props, None)
+
+    def test_split_nodes_delimiter(self):
+
+        bold = TextNode("This is important information", TextType.BOLD)
+        no_split = split_nodes_delimiter([bold], "**", TextType.BOLD)
+        self.assertEqual(no_split[0], bold)
+
+        text_bold = TextNode("Click the **'Submit'** button to proceed.", TextType.NORMAL)
+        bold_split = split_nodes_delimiter([text_bold], "**", TextType.BOLD)
+        self.assertEqual(bold_split[1].text, "'Submit'")
+
+        text_italics = TextNode("Check some _fancy italics_", TextType.NORMAL)
+        italic_split = split_nodes_delimiter([text_italics], "_", TextType.ITALIC)
+        self.assertEqual(italic_split[1].text, "fancy italics")
+
+        text_code = TextNode('Use `git commit -m "Your message"` to save changes', TextType.NORMAL)
+        code_split = split_nodes_delimiter([text_code], "`", TextType.CODE)
+        self.assertEqual(code_split[1].text, 'git commit -m "Your message"')
+
+        text_double_bold = TextNode("**Warning**: This action **cannot** be undone!", TextType.NORMAL)
+        double_split = split_nodes_delimiter([text_double_bold], "**", TextType.BOLD)
+        self.assertEqual(double_split[1].text, "Warning")
+        self.assertEqual(double_split[3].text, "cannot")
 
 if __name__ == '__main__':
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     NORMAL = "normal"
     ITALIC = "italic"
@@ -23,6 +25,49 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL:
+            return LeafNode(None, text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception(f"Node's text type {text_node.text_type} not supported.")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if (delimiter == '**' and text_type is not TextType.BOLD
+        or delimiter == '_' and text_type is not TextType.ITALIC
+        or delimiter == '`' and text_type is not TextType.CODE):
+        raise ValueError(f"Delimiter {delimiter} does not match {text_type}.")
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+        else:
+            text_split = node.text.split(delimiter)
+            if len(text_split) in [1, 2]:
+                raise ValueError(f"Delimiter/s {delimiter} not found")
+            for idx, text in enumerate(text_split):
+                if (idx+1) % 2 != 0:
+                    new_nodes.append(TextNode(text, TextType.NORMAL))
+                else:
+                    new_nodes.append(TextNode(text, text_type))
+    return new_nodes
+
 if __name__ == '__main__':
-    tn = TextNode("*Bold*", TextType.BOLD)
-    print(tn)
+    bold_node = TextNode("**important notice**", TextType.BOLD)
+    link_node = TextNode("[link](https://www.example.com)", TextType.LINK)
+    img_node = TextNode("![alt text for image](url/of/image.jpg)", TextType.IMAGE)
+    bold_node_1 = TextNode("This is **text** with a `code block` word", TextType.NORMAL)
+    bold_node_2 = TextNode("And this is more **important** with a **bold text** word", TextType.NORMAL)
+    new_nodes = split_nodes_delimiter([bold_node, bold_node_1, link_node, img_node, bold_node_2], "**", TextType.BOLD)
+    print(new_nodes)
