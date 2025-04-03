9991033a286c5d3c1e627bb692220a3dfbf9ae19
charlesaraya <charlesaraya@gmail.com>
2025-04-03 16:50:53 +0200

    fix: add img special case; a leaf html node with no closing tag and empty value

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,11 +21,13 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if not self.value:
+        if not self.value and self.tag != 'img':
             raise ValueError("A leaf node must have a value")
 
         if not self.tag:
             return self.value
+        if self.tag == 'img':
+            return f"<{self.tag}{self.props_to_html()}>"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
 class ParentNode(HTMLNode):
